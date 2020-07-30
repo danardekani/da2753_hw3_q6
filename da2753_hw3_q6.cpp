585e9f4fb2abc7a5cf31b5f03a24ac74c83ab98d
Daniel <ardekanicapital@gmail.com>
2020-07-30 12:03:04 -0400

    updated script. user provides input values, and program calculates phone call.

--- da2753_hw3_q6.cpp
+++ da2753_hw3_q6.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 int main()
 {
-    int minutes;
-    string day, callTime, hrs;
+    int minutes, hour;
+    double callRate;
+    string day, callTime, hours;
 
     cout << "Please enter time in 24-hr notation, e.g. if 1:30 P.M., then input as 13:30: " << endl;
     cin >> callTime;
@@ -15,11 +18,30 @@ int main()
     cin >> minutes;
 
     //Extract the hours from the callTime;
-    hrs = callTime.substr(0,2);
+    hours = callTime.substr(0,2);
 
-    cout << callTime << endl;
-    cout << minutes << endl;
-    cout << day << endl;
-    cout << hrs << endl;
+    //Convert string to int
+    stringstream time(hours);
+
+    //convert string(hours) to int(hour)
+    time >> hour;
+
+    if (day == "Sa" || day == "Su")
+    {
+        callRate = minutes * .15;
+    } else if (day == "Mo" || day == "Tu" || day == "We" || day == "Th" || day == "Fr")
+    {
+        if (hour >= 8 && hour <= 18)
+        {
+            callRate = minutes * .40;
+        } else if (hour < 8 || hour > 18)
+        {
+            callRate = minutes * .25;
+        } else
+            return 0;
+    }
+
+    cout << "The cost of the call is " << callRate << endl;
+
     return 0;
 }
